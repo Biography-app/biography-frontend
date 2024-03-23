8cb46a0c02de748092acdd308097c021dc4f7aa5
cherry <reebon7@gmail.com>
2024-03-23 16:35:05 +0900

    chore: 개발환경 세팅을 위한 수정 및 삭제

--- api/signinApi.js
+++ api/signinApi.js
@@ -3,6 +3,7 @@ import EncryptedStorage from 'react-native-encrypted-storage';
 
 const baseURl = '';
 
+// 일반로그인 
 export const signIn = async (email, password) => {
   try {
     const response = await axios.post(`${baseURl}/auth/sign-in`, {
@@ -14,6 +15,9 @@ export const signIn = async (email, password) => {
     const access_token = response.data.access_token;
     const refresh_token = response.data.refresh_token;
 
+    await EncryptedStorage.setItem('accessToken', response.data.access_token);
+    await EncryptedStorage.setItem('refreshToken', response.data.refresh_token);
+
     console.log(
       'success',
       success,
@@ -23,24 +27,31 @@ export const signIn = async (email, password) => {
       refresh_token,
     );
 
-    await EncryptedStorage.setItem('accessToken', response.data.access_token);
-    await EncryptedStorage.setItem('refreshToken', response.data.refresh_token);
-
     //response 응답 자체를 리턴하고,
   } catch (error) {
     console.error('일반 로그인 중 실패 : ', error);
   }
 };
 
+
 //카카오 로그인은 잘 모르겠다.
 export const kakaoLogin = async () => {
   try {
-    const response = await axios.post(`${baseUrl}/auth/kakao/callback`, {
-      token_type,
+    const response = await axios.get(`${baseURl}/auth/kakao/callback`);
+
+    const access_token = response.data.access_token;
+    const refresh_token = response.data.refresh_token;
+
+    await EncryptedStorage.setItem('accessToken', response.data.access_token);
+    await EncryptedStorage.setItem('refreshToken', response.data.refresh_token);
+
+    console.log(
+      'access_token: ',
       access_token,
-      expires_in,
-      code,
-    });
+      'refresh_token: ',
+      refresh_token,
+    );
+
   } catch (error) {}
 };
 
